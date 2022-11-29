246ba9ef6ce0ec0b3517e6db5222ab549ab0a405
ShonChassel <gershoni1234@gmail.com>
2022-11-29 20:49:03 +0200

    first

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,22 +1,26 @@
 <template>
-    <header class="main-header">
-        <div class="logo">
-            <img src="../assets/svg/logo.svg" alt="" />
-        </div>
-        <app-search></app-search>
-        <button class="user">
-            
-            <img src="../assets/svg/user.svg" alt="" />
-        </button>
-    </header>
+  <header class="main-header">
+    <div class="logo">
+      <img src="../assets/svg/logo.svg" alt="" />
+    </div>
+    <app-search></app-search>
+
+    <div>
+      <div><div>Switch to hosting</div></div>
+
+      <button class="user">
+        <img src="../assets/img/nadir.PNG" alt="" />
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>
 import appSearch from "./app-search.vue";
 
 export default {
-    components: {
-        appSearch,
-    },
+  components: {
+    appSearch,
+  },
 };
 </script>
