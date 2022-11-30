3699a6e1d500455aa93c76786cfb364c14423dd3
ShonChassel <gershoni1234@gmail.com>
2022-11-30 18:34:40 +0200

    first

--- src/cmps/filter-modal.vue
+++ src/cmps/filter-modal.vue
@@ -1,141 +1,196 @@
 <template>
-  <div class="filter-modal-container">
-
-    <div class="filter-modal">
+<div class="form-filter">
 
       <div class="form-header">
-        <button @click="$emit('close')">X</button>
-        <p>Filters</p>
+        <div class="close-filter">
+          <img @click="$emit('close')" src="../assets/svg/close.svg" alt="">
+        </div>
+        <h2>Filters</h2>
       </div>
 
-      <section>
-        <h2>Price range</h2>
-        <p>The average nightly price is {{ formattedPrice }}</p>
-        <!-- <chart></chart> -->
+  <div class="form-wrapper">
 
-        <div class="range">
-          <input type="text" v-model.number="filterBy.maxPrice" />
-          -
-          <input type="text" v-model.number="filterBy.minPrice" />
-        </div>
-      </section>
+     <div class="form-price">
+       <h2>Price range</h2>
+       <p>The average nightly price is $206</p>
+       <!-- <chart></chart> -->
+       
+      <div class="form-inputs">
+          <div class="price-inner">
+            <label for="min">min price</label>
+            <div class="flex form-input">
+              <span>$</span>
+              <input name="min" type="text" v-model.number="filterBy.maxPrice" min="25$" />
+            </div>
+         </div>
 
-      <hr />
-      <section>
+          <div class="between-prices">-</div>
+
+          <div class="price-inner">
+            <label for="max"> max price</label>
+            <div class="flex form-input">
+              <span>$</span>
+              <input type="text" v-model.number="filterBy.minPrice" max="$800" />
+            </div>
+         </div>
+      </div>
+    </div>
+      
+      <section class="Type-of-place">
         <h2>Type of place</h2>
-        <input
-          type="checkbox"
-          name="entire"
-          v-model="filterBy.type"
-          value="Entire place"
-        />
-        <label for="entire">Entire place</label>
-
-        <input
-          type="checkbox"
-          name="private"
-          v-model="filterBy.type"
-          value="Private room"
-        />
-        <label for="entire">Private room</label>
-
-        <input
-          type="checkbox"
-          name="shared"
-          v-model="filterBy.type"
-          value="Shared room"
-        />
-        <label for="shared">Shared room</label>
+        
+      <div class="first-tow">
+        <div class="checkbox">
+          <input type="checkbox" name="entire" v-model="filterBy.type" value="Entire place"/>
+          <div class="label-p">
+            <label>Entire place</label>
+            <p>A place all to yourself</p>
+          </div>
+        </div>
+        
+        <div class="checkbox">
+          <input type="checkbox" name="private" v-model="filterBy.type" value="Private room" />
+          <div class="label-p">
+            <label for="entire">Private room</label>
+            <p>Your own room in a home or a hotel, plus some shared common spaces</p>
+          </div>
+        </div>
+      </div>
+        <div class="checkbox">
+          <input type="checkbox" name="shared" v-model="filterBy.type" value="Shared room"/>
+          <div class="label-p">
+          <label for="shared">Shared room</label>
+          <p>A sleeping space and common areas that may be shared with others</p>
+        </div>
+        </div>
       </section>
 
       <hr />
-      <section>
+
+    <div class="form-rooms-and-beds">
         <h2>Rooms and beds</h2>
-
+        <h3>BedRooms</h3>
         <div>
-          <p>Bedrooms</p>
-          <button
-            @click="setRoomsBeds('bedrooms', num)"
-            v-for="num in 10"
-            :key="num"
-          >
-            {{ num }}
-          </button>
-        </div>
-
+          <button class="btn-btn">Any</button>
+          <button class="btn-btn" @click="setRoomsBeds('bedrooms', num)" v-for="num in 8" :key="num" > {{ num }}</button>
+       </div>
+        
         <div>
-          <p>Beds</p>
-          <button
-            @click="setRoomsBeds('beds', num)"
-            v-for="num in 10"
-            :key="num"
-          >
-            {{ num }}
-          </button>
-        </div>
-
-        <div>
-          <p>Bathrooms</p>
-          <button
-            @click="setRoomsBeds('bedrooms', num)"
-            v-for="num in 10"
-            :key="num"
-          >
-            {{ num }}
-          </button>
-        </div>
-      </section>
-
+          <h3>beds</h3>
+          <button class="btn-btn">Any</button>
+          <button class="btn-btn" @click="setRoomsBeds('beds', num)" v-for="num in 8" :key="num" >{{ num }}</button>
+      </div>
+      
+      <div>
+        <h3>Bathrooms</h3>
+        <button class="btn-btn">Any</button>
+        <button class="btn-btn" @click="setRoomsBeds('bedrooms', num)" v-for="num in 8" :key="num" >
+        {{ num }}
+      </button>
+     </div>
+  </div>
+  
       <hr />
-      <section>
-        <h2>Property type</h2>
-        <button @click="setPropertyType('house')">House</button>
-        <button @click="setPropertyType('apartment')">Apartment</button>
-        <button @click="setPropertyType('guesthouse')">Guesthouse</button>
-        <button @click="setPropertyType('hotel')">Hotel</button>
-      </section>
+      
+    <div class="form-property-type">
+       <h2>Property type</h2>
+       <div class="flex">
 
-      <section>
-        <h2>Amenities</h2>
-        <div class="checkboxes">
-          <input type="checkbox" name="wifi" />
-          <label for="wifi">Wifi</label>
-          <input type="checkbox" name="washer" />
-          <label for="washer">Washer</label>
-          <input type="checkbox" name="ac" />
-          <label for="ac">Ac</label>
-          <input type="checkbox" name="kitchen" />
-          <label for="kitchen">Kitchen</label>
+        <div class="property-type" @click="setPropertyType('house')">
+            <div>
+              <img src="https://a0.muscache.com/pictures/4d7580e1-4ab2-4d26-a3d6-97f9555ba8f9.jpg" alt="">
+              <span>House</span>
+            </div>
         </div>
-      </section>
 
-      <section>Booking options</section>
+        <div class="property-type" @click="setPropertyType('apartment')">
+            <div>
+              <img src="https://a0.muscache.com/pictures/21cfc7c9-5457-494d-9779-7b0c21d81a25.jpg" alt="">
+              <span>Apartment</span>
+            </div>
+        </div>
 
-      <label>
-        Instant Book
-        <input type="checkbox" />
-      </label>
+        <div class="property-type" @click="setPropertyType('guesthouse')">
+            <div>
+              <img src="https://a0.muscache.com/pictures/6f261426-2e47-4c91-8b1a-7a847da2b21b.jpg" alt="">
+              <span>guesthouse</span>
+            </div>
+        </div>
 
-      <label>
-        Self check-in
-        <input type="checkbox" />
-      </label>
+        <div class="property-type" @click="setPropertyType('hotel')">
+            <div>
+              <img src="https://a0.muscache.com/pictures/64b27fed-56a1-4f03-950a-d8da08efb428.jpg" alt="">
+              <span>Hotel</span>
+            </div>
+        </div>
+        </div>
+    </div>
+      
+      <div class="form-amenities">
+        <div class="form-title-amenities">
+          <h2>Amenities</h2>
+        </div>
 
-        <footer>
-          <button>Clear all</button>
-          <button @click="filterHouses">Show {{ totalHouses }} homes</button>
-        </footer>
+        <div class="essentials">
+          <h3>Essentials</h3>
+          <div>
+            <div class="el-checkbox-group">
+              <label class="el-checkbox">
+                <span class="el-checkbox__input">
+                  <input class="el-checkbox__original" type="checkbox" aria-hidden="false" value="Wifi">
+                  <span class="el-checkbox__inner"></span>
+                </span>
+                <span class="el-checkbox__label">Wifi</span>
+              </label>
+              <label class="el-checkbox">
+                <span class="el-checkbox__input">
+                  <input class="el-checkbox__original" type="checkbox" aria-hidden="false" value="Kitchen">
+                  <span class="el-checkbox__inner"></span>
+                </span>
+                <span class="el-checkbox__label">Kitchen</span>
+              </label>
+              <label class="el-checkbox">
+                <span class="el-checkbox__input">
+                  <input class="el-checkbox__original" type="checkbox" aria-hidden="false" value="Washer">
+                  <span class="el-checkbox__inner"></span>
+                </span><span class="el-checkbox__label">Washer</span>
+              </label>
+              <label class="el-checkbox">
+                <span class="el-checkbox__input">
+                  <input class="el-checkbox__original" type="checkbox" aria-hidden="false" value="TV">
+                  <span class="el-checkbox__inner"></span>
+                </span>
+                <span class="el-checkbox__label">TV</span>
+              </label>
+              <label class="el-checkbox"><span class="el-checkbox__input">
+                <input class="el-checkbox__original" type="checkbox" aria-hidden="false" value="Air conditioning">
+                <span class="el-checkbox__inner"></span>
+              </span>
+              <span class="el-checkbox__label">Air conditioning</span>
+            </label></div>
+          </div>
+        </div>
+      </div>
+
+      
+      
+    </div>
+    
+    <div class="form-footer">
+      <button>Clear all</button>
+      <button @click="filterHouses">Show {{ totalHouses }} homes</button>
     </div>
   </div>
-</template>
+    
+  </template>
 <script>
 export default {
   components: {},
   data() {
     return {
       filterBy: {
-        maxPrice: 0,
-        minPrice: 0,
+        maxPrice: 25,
+        minPrice: 800,
         type: [],
         roomsBeds: {
           bedrooms: 0,
