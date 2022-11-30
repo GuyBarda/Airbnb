ccf6f06c71e7956183757bce6234e1e48e837915
Guy Barda <guy.barda.1234@gmail.com>
2022-11-30 09:13:53 +0200

    details

--- src/cmps/house-preview.vue
+++ src/cmps/house-preview.vue
@@ -1,21 +1,21 @@
 <template>
-    <router-link :to="'house/'+house._id">
-    <article class="house-preview">
-        <heart-icon @click.prevent="isMark = !isMark" :class="{mark: isMark}" class="heart-btn"/>
-        <!-- <div class="carousel"> -->
+    <router-link :to="'house/' + house._id">
+        <article class="house-preview">
+            <heart-icon @click.prevent="isMark = !isMark" :class="{ mark: isMark }" class="heart-btn" />
+            <!-- <div class="carousel"> -->
             <!-- <button class="arrow-btn arrow-left"><arrow-left /></button> -->
-            <img-carousel @click.prevent :imgs="house.imgUrls"/>
+            <!-- <img-carousel @click.prevent :imgs="house.imgUrls"/> -->
             <!-- <img :src="house.imgUrls[0]"> -->
             <!-- <img :src="house.assets[0]" alt="house"> -->
             <!-- <button class="arrow-btn arrow-right"><arrow-right /></button> -->
-        <!-- </div> -->
-        <section>
-        <p class="location">{{ location }}</p>
-        <p>{{ house.name }}</p>
-        <p><span class="price">{{ formattedPrice }}</span> night</p>
-        <p class="rate"><star-icon />&nbsp;{{rate}}</p>
-        </section>
-    </article>
+            <!-- </div> -->
+            <section>
+                <p class="location">{{ location }}</p>
+                <p>{{ house.name }}</p>
+                <p><span class="price">{{ formattedPrice }}</span> night</p>
+                <p class="rate"><star-icon />&nbsp;{{ rate }}</p>
+            </section>
+        </article>
     </router-link>
 </template>
 
@@ -24,24 +24,24 @@ import arrowLeft from '../assets/svg/arrow-left.vue'
 import arrowRight from '../assets/svg/arrow-right.vue'
 import starIcon from '../assets/svg/star.vue'
 import heartIcon from '../assets/svg/heart.vue'
-import imgCarousel from '../cmps/img-carousel.vue'
+// import imgCarousel from '../cmps/img-carousel.vue'
 
 export default {
     props: {
         house: Object
     },
-    data(){
-        return{
+    data() {
+        return {
             isMark: false,
         }
     },
     computed: {
-        rate(){
-            let sum = this.house.reviews.reduce((acc,{rate}) => acc+= rate,0)
-            sum /=this.house.reviews.length
+        rate() {
+            let sum = this.house.reviews.reduce((acc, { rate }) => acc += rate, 0)
+            sum /= this.house.reviews.length
             return `${sum} (${this.house.reviews.length})`
         },
-        location(){
+        location() {
             return `${this.house.loc.country},${this.house.loc.city}`
         },
         distanceFromMe() {
@@ -60,7 +60,7 @@ export default {
         arrowRight,
         starIcon,
         heartIcon,
-        imgCarousel,
+        // imgCarousel,
     }
 };
 </script>
