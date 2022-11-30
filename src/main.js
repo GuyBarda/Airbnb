1a0e3030f5dfa5cf09fe7acf8bef02bcf0969fa8
Guy Barda <guy.barda.1234@gmail.com>
2022-11-30 18:40:22 +0200

    details

--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-<<<<<<< HEAD
-import {store} from './store/store.js'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-=======
 import { store } from './store/store.js';
->>>>>>> e369b8b7b97fffb49f567f131753abacafca077e
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 import './assets/main.scss';
 import './assets/setup/_typography.scss';
 
 const app = createApp(App);
 
-app.use(ElementPlus)
+app.use(ElementPlus);
 app.use(router);
 app.use(store);
 app.mount('#app');
