746c97315c9fe71126039e6dc052afe45c26e461
Eyal Yehiel <eyalyhi@gmail.com>
2022-12-03 12:43:01 +0200

    updated edit page

--- src/cmps/search-modal.vue
+++ src/cmps/search-modal.vue
@@ -7,8 +7,9 @@
         class="search-zone destination"
       >
         <label>Where</label>
-        <input class="dest-input"
-        :class="{set: filterBy.destination}"
+        <input
+          class="dest-input"
+          :class="{ set: filterBy.destination }"
           type="text"
           v-model="filterBy.destination"
           placeholder="Search destinations"
@@ -27,8 +28,8 @@
         >
           <label>Check in</label>
           <input
-          :class="{set: formattedStartDate}"
-          :value="formattedStartDate"
+            :class="{ set: formattedStartDate }"
+            :value="formattedStartDate"
             class="date-input"
             type="text"
             placeholder="Add dates"
@@ -52,8 +53,8 @@
         >
           <label>Check out</label>
           <input
-          :class="{set: formattedEndDate}"
-          :value="formattedEndDate"
+            :class="{ set: formattedEndDate }"
+            :value="formattedEndDate"
             class="date-input"
             type="text"
             placeholder="Add dates"
@@ -61,7 +62,7 @@
           />
         </div>
       </section>
-            <span></span>
+      <span></span>
 
       <div
         @click.prevent="openZone('Who')"
@@ -71,7 +72,7 @@
         <label>Who</label>
         <input
           :value="guestsCount"
-          :class="{set: guestsCount}"
+          :class="{ set: guestsCount }"
           class="guest-input"
           type="text"
           placeholder="Add guests"
@@ -94,8 +95,7 @@ import reactiveBtn from "./reactive-btn.vue";
 import destinationModal from "./destination-modal.vue";
 import datePicker from "./date-picker.vue";
 import guestsModal from "./guests-modal.vue";
-import { ref } from 'vue'
-
+import { ref } from "vue";
 
 export default {
   props: {
@@ -106,7 +106,7 @@ export default {
   data() {
     return {
       isSelect: "",
-      dates: ref(''),
+      dates: ref(""),
       filterBy: {
         destination: "",
         dates: {
@@ -131,8 +131,18 @@ export default {
     }
   },
   methods: {
-    setSearch(){
-      this.$store.commit({type: 'setFilter', filterBy: {...this.filterBy}})
+    setSearch() {
+      this.$store.commit({ type: "setFilter", filterBy: { ...this.filterBy } });
+      const {adults,children,pets,infants} = this.filterBy.guests
+      const sum = adults + children + pets + infants
+      const queryStringParams = `?dest=${this.filterBy.destination}&guests=${sum}`;
+      const newUrl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname +
+        queryStringParams;
+      window.history.pushState({ path: newUrl }, "", newUrl);
     },
     openZone(val) {
       this.isSelect = val;
@@ -140,37 +150,50 @@ export default {
     },
     setDestination(dest) {
       this.filterBy.destination = dest;
-      this.$emit('setDest',dest)
+      this.$emit("setDest", dest);
     },
     setGuests(guests) {
       this.filterBy.guests = { ...guests };
     },
-    setDates(dates){
-      this.filterBy.dates.start = this.dates["0"]
-      this.filterBy.dates.end = this.dates["1"]
-      let str = this.formattedStartDate + ' - ' + this.formattedEndDate
+    setDates(dates) {
+      this.filterBy.dates.start = this.dates["0"];
+      this.filterBy.dates.end = this.dates["1"];
+      let str = this.formattedStartDate + " - " + this.formattedEndDate;
       console.log(str);
-      this.$emit('setDate',str)
-    }
+      this.$emit("setDate", str);
+    },
   },
   computed: {
     guestsCount() {
-      const {adults,children,infants,pets} = this.filterBy.guests
-      let str = adults || children ? `${adults + children} ${adults + children !== 1 ? 'guests' : 'guest'},` : ''
-      str += infants ? ` ${infants} ${infants !== 1 ? 'infants': 'infant'},` : ''
-      str += pets ? ` ${pets} ${pets !== 1 ? 'pets' : 'pet'}` : ''
-      return str
+      const { adults, children, infants, pets } = this.filterBy.guests;
+      let str =
+        adults || children
+          ? `${adults + children} ${
+              adults + children !== 1 ? "guests" : "guest"
+            },`
+          : "";
+      str += infants
+        ? ` ${infants} ${infants !== 1 ? "infants" : "infant"},`
+        : "";
+      str += pets ? ` ${pets} ${pets !== 1 ? "pets" : "pet"}` : "";
+      return str;
     },
-    formattedStartDate(){
-        let date = new Date(this.filterBy.dates.start).toLocaleDateString('en-us', { month:"short", day:"numeric"}) 
-        if(date === 'Invalid Date') return ''
-        return date
+    formattedStartDate() {
+      let date = new Date(this.filterBy.dates.start).toLocaleDateString(
+        "en-us",
+        { month: "short", day: "numeric" }
+      );
+      if (date === "Invalid Date") return "";
+      return date;
     },
-    formattedEndDate(){
-        let date = new Date(this.filterBy.dates.end).toLocaleDateString('en-us', { month:"short", day:"numeric"}) 
-        if(date === 'Invalid Date') return ''
-        return date
-    }
+    formattedEndDate() {
+      let date = new Date(this.filterBy.dates.end).toLocaleDateString("en-us", {
+        month: "short",
+        day: "numeric",
+      });
+      if (date === "Invalid Date") return "";
+      return date;
+    },
   },
   components: {
     reactiveBtn,
--- src/views/house-edit.vue
+++ src/views/house-edit.vue
@@ -28,21 +28,22 @@
                         <upload-img v-for="x in 5" :key="x" :idx="x" />
                     </section>
                     <section class="below-imgs">
-                        <div style="display: flex; gap: 5px;">
+                        <div style="display: flex; justify-content: space-between; align-items:center">
                             <label for="capacity">Capacity</label>
                             <input type="text" name="capacity">
-                            <label for="stay-type">Stay type</label>
+                            <label for="stay-type">Stay type:</label>
                             <select name="stay-type">
                                 <option value="entire place">Entire place</option>
                                 <option value="private room">Private room</option>
                                 <option value="shared room">Shared room</option>
                             </select>
-                            <label for="property-type">Property type</label>
+                            <label for="property-type">Property type:</label>
                             <select name="property-type">
                                 <option value="House">House</option>
                                 <option value="apartment">Apartment</option>
                                 <!-- <option value="Shared room">Shared room</option> -->
                             </select>
+                            <div>Price: <input style="width:40px" type="text"> /night</div>
                         </div>
                         <div>
                             <h3>Description</h3>
@@ -60,6 +61,9 @@
                                 </div>
                             </div>
                         </div>
+                        <section class="confirm-area">
+                            <button class="confirm-btn" type="button">Save</button>
+                        </section>
                     </section>
                 </section>
             </section>
@@ -71,12 +75,18 @@
 import starIcon from '../assets/svg/star.vue'
 import plusIcon from '../assets/svg/plus.vue'
 import uploadImg from '../cmps/upload-img.vue'
+import {houseService} from '../services/house-service-local.js'
 
 export default {
     data() {
         return {
-            allAmenities: ['air', 'cooking', 'dedicated', 'free', 'garden', 'kitchen', 'mountain', 'private hot tub', 'private pation', 'smoking', 'tv', 'washer', 'wifi']
+            allAmenities: ['air', 'cooking', 'dedicated', 'free', 'garden', 'kitchen', 'mountain', 'private-hot-tub', 'private-pation', 'smoking', 'tv', 'washer', 'wifi']
+            house: null,
         }
+    },
+    async created(){
+        const { id } = this.$route.params
+        this.house = await houseService.getById(id)
     },
 
     components: {
