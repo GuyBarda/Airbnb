a9f7cdadca0f7c248899102dc7e1e13b08f7e3fb
Guy Barda <guy.barda.1234@gmail.com>
2022-12-04 18:11:29 +0200

    filter

--- src/cmps/filter-modal.vue
+++ src/cmps/filter-modal.vue
@@ -145,7 +145,27 @@
             <div class="form-property-type">
                 <h2>Property type</h2>
                 <div class="flex">
-                    <div class="property-type" @click="setPropertyType('house')">
+                    <input type="checkbox" v-model="filterBy.PropertyType" name="house">
+                    <label for="house" class="property-type">
+                        <img src="https://a0.muscache.com/pictures/4d7580e1-4ab2-4d26-a3d6-97f9555ba8f9.jpg" alt="" />
+                        <span>House</span>
+                    </label>
+                    <input type="checkbox" v-model="filterBy.PropertyType" name="house">
+                    <label for="house" class="property-type">
+                        <img src="https://a0.muscache.com/pictures/21cfc7c9-5457-494d-9779-7b0c21d81a25.jpg" alt="" />
+                        <span>Apartment</span>
+                    </label>
+                    <input type="checkbox" v-model="filterBy.PropertyType" name="house">
+                    <label for="house" class="property-type">
+                        <img src="https://a0.muscache.com/pictures/6f261426-2e47-4c91-8b1a-7a847da2b21b.jpg" alt="" />
+                        <span>guesthouse</span>
+                    </label>
+                    <input type="checkbox" v-model="filterBy.PropertyType" name="house">
+                    <label for="house" class="property-type">
+                        <img src="https://a0.muscache.com/pictures/64b27fed-56a1-4f03-950a-d8da08efb428.jpg" alt="" />
+                        <span>Hotel</span>
+                    </label>
+                    <!-- <div class="property-type" @click="setPropertyType('house')">
                         <div>
                             <img src="https://a0.muscache.com/pictures/4d7580e1-4ab2-4d26-a3d6-97f9555ba8f9.jpg"
                                 alt="" />
@@ -175,7 +195,7 @@
                                 alt="" />
                             <span>Hotel</span>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
 
