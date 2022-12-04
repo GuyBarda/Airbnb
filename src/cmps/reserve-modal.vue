3cf70b9f6dc20fea2e7f26adf2db1c8e760afab3
Guy Barda <guy.barda.1234@gmail.com>
2022-12-04 15:23:03 +0200

    details fix

--- src/cmps/reserve-modal.vue
+++ src/cmps/reserve-modal.vue
@@ -10,30 +10,15 @@
       <div class="picker-container">
         <div @click="showDatePicker = true" class="check-in picker">
           <label for="check-in">CHECK-IN</label>
-          <input
-            type="text"
-            placeholder="MM/DD/YYYY"
-            :value="formatedStartDate"
-          />
+          <input type="text" placeholder="MM/DD/YYYY" :value="formatedStartDate" />
         </div>
         <section class="date-picker-container">
-        <el-date-picker
-        :teleported="false"
-        @change="setDates"
-        v-model="dates"
-          popper-class="date-picker-reserve"
-          type="daterange"
-          start-placeholder="Start date"
-          end-placeholder="End date"
-        />
+          <el-date-picker :teleported="false" @change="setDates" v-model="dates" popper-class="date-picker-reserve"
+            type="daterange" start-placeholder="Start date" end-placeholder="End date" />
         </section>
         <div @click="showDatePicker = true" class="check-out picker">
           <label for="check-out">CHECK-OUT</label>
-          <input
-            type="text"
-            placeholder="MM/DD/YYYY"
-            :value="formatedEndDate"
-          />
+          <input type="text" placeholder="MM/DD/YYYY" :value="formatedEndDate" />
           <!-- <button v-if="order.endDate" @click="order.endDate = ''">
                         <img src="../assets/svg/close.svg" alt="">
                     </button> -->
@@ -44,18 +29,12 @@
           <!-- <button v-if="order.endDate" @click="order.endDate = ''">
                         <img src="../assets/svg/close.svg" alt="">
                     </button> -->
-          <guests-modal
-            @setGuests="setGuests"
-            :class="{ open: showGuestPicker }"
-          ></guests-modal>
+          <guests-modal @setGuests="setGuests" :class="{ open: showGuestPicker }"></guests-modal>
         </div>
       </div>
       <button @mousemove="hoverEffect" class="btn-reserve">Reserve</button>
 
-      <div
-        style="display: flex; gap: 25px; flex-direction: column"
-        v-if="order.startDate && order.endDate"
-      >
+      <div style="display: flex; gap: 25px; flex-direction: column" v-if="order.startDate && order.endDate">
         <p style="text-align: center">You won't be charged yet</p>
         <div class="prices">
           <p>{{ formattedPerNightPrice }} x {{ getTotalDays }} nights</p>
@@ -76,6 +55,7 @@
 
 <script>
 import { orderService } from '../services/order-service-local.js'
+import { ref } from "vue";
 
 import reviewAverage from "../cmps/review-average.vue";
 import reactiveBtn from "./reactive-btn.vue";
@@ -109,38 +89,37 @@ export default {
     setGuests(guests) {
       this.order.guests = { ...guests };
     },
-    methods: {
-        addOrder() {
-            this.$store.commit({ type: "toggleSuccessModal", bool: true });
-            this.$store.dispatch({ type: 'addOrder', order: this.order })
-        },
-        totalDays() {
-            const date1 = new Date(this.order.startDate);
-            const date2 = new Date(this.order.endDate);
-            const diffTime = Math.abs(date2 - date1);
-            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-            return diffDays
-        },
-        format(num) {
-            const formatter = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-            });
-            return formatter.format(num)
-        },
-        formatDate(date) {
-            const DATE = new Date(date)
-            return `${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`
-            return DATE.getDate()
-        },
-        hoverEffect(ev) {
-            const button = ev.target
-            const { x, y } = button.getBoundingClientRect();
-            button.style.setProperty("--x", ev.clientX - x + 'px');
-            button.style.setProperty("--y", ev.clientY - y + 'px');
-        }
-
+    addOrder() {
+      this.$store.commit({ type: "toggleSuccessModal", bool: true });
+      this.$store.dispatch({ type: 'addOrder', order: this.order })
     },
+    totalDays() {
+      const date1 = new Date(this.order.startDate);
+      const date2 = new Date(this.order.endDate);
+      const diffTime = Math.abs(date2 - date1);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      return diffDays
+    },
+    format(num) {
+      const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      });
+      return formatter.format(num)
+    },
+    formatDate(date) {
+      const DATE = new Date(date)
+      return `${DATE.getDate()}/${DATE.getMonth() + 1}/${DATE.getFullYear()}`
+      return DATE.getDate()
+    },
+    hoverEffect(ev) {
+      const button = ev.target
+      const { x, y } = button.getBoundingClientRect();
+      button.style.setProperty("--x", ev.clientX - x + 'px');
+      button.style.setProperty("--y", ev.clientY - y + 'px');
+    }
+
+
   },
   computed: {
     guestsCount() {
@@ -148,9 +127,8 @@ export default {
       const { adults, children, infants, pets } = this.order.guests;
       let str =
         adults || children
-          ? `${adults + children} ${
-              adults + children !== 1 ? "guests" : "guest"
-            },`
+          ? `${adults + children} ${adults + children !== 1 ? "guests" : "guest"
+          },`
           : "";
       str += infants
         ? ` ${infants} ${infants !== 1 ? "infants" : "infant"},`
