fa19496c1aefd0606dd7e6600e16e93fc605b38f
Guy Barda <guy.barda.1234@gmail.com>
2022-12-04 11:36:29 +0200

    edit

--- src/views/house-edit.vue
+++ src/views/house-edit.vue
@@ -22,57 +22,41 @@
               <input type="text" v-model="house.name" placeholder="Stay name" />
             </h2>
             <div>
-              <p><star-icon style="display: inline" />{{ rate }}</p>
-              <p>
-                &#183;Address <input type="text" v-model="house.loc.address" />
-              </p>
+
+              <label>
+                Address <input type="text" v-model="house.loc.address" />
+              </label>
             </div>
           </section>
           <section class="imgs-container">
-            <upload-img
-              v-for="x in 5"
-              @setImg="setImg"
-              :img="house.imgUrls[x]"
-              :key="x"
-              :idx="x"
-            />
+            <upload-img v-for="x in 5" @setImg="setImg" :img="house.imgUrls[x]" :key="x" :idx="x" />
           </section>
           <section class="below-imgs">
-            <div
-              style="
+            <div style="
                 display: flex;
                 justify-content: space-between;
-                align-items: center;
-              "
-            >
+                align-items: center;">
               <label for="capacity">Capacity</label>
               <input type="text" name="capacity" v-model="house.capacity" />
               <label for="stay-type">Stay type:</label>
-              <select name="stay-type">
-                <option
-                  :selected="(house.type[0] = 'entire place')"
-                  value="entire place"
-                >
+              <select name="stay-type" v-model="house.stayType">
+                <option value="Entire place">
                   Entire place
                 </option>
-                <option
-                  :selected="(house.type[0] = 'private room')"
-                  value="private room"
-                >
+                <option value="Private room">
                   Private room
                 </option>
-                <option
-                  :selected="(house.type[0] = 'shared room')"
-                  value="shared room"
-                >
+                <option value="Shared room">
                   Shared room
                 </option>
               </select>
               <label for="property-type">Property type:</label>
-              <select name="property-type">
+              <select name="property-type" v-model="house.propertyType">
                 <option value="House">House</option>
                 <option value="apartment">Apartment</option>
-                <!-- <option value="Shared room">Shared room</option> -->
+                <option value="Guesthouse">Guesthouse</option>
+                <option value="Hotel">Hotel</option>
+
               </select>
               <div>
                 Price:
@@ -88,22 +72,13 @@
               <h3>Amenities</h3>
               <div class="house-amenities">
                 <div v-for="a in allAmenities" style="display: flex; gap: 13px">
-                  <input
-                    @change="checkAmenities(a)"
-                    type="checkbox"
-                    :name="a"
-                    :checked="house.amenities.includes(a)"
-                  />
-                  <img
-                    :src="`/src/assets/svg/amenities/${a
-                      .split(' ')[0]
-                      .toLowerCase()}.svg`"
-                    alt="np"
-                    style="width: 1.2em"
-                  />
+                  <input @change="checkAmenities(a)" type="checkbox" :name="a" :checked="house.amenities.includes(a)" />
+                  <img :src="`/src/assets/svg/amenities/${a
+                  .split(' ')[0]
+                  .toLowerCase()}.svg`" alt="np" style="width: 1.2em" />
                   <!-- <img src="../assets/svg/amenities/tv.svg" alt=""> -->
                   <label :for="a">{{
-                    a.charAt(0).toUpperCase() + a.slice(1).replaceAll("-", " ")
+                      a.charAt(0).toUpperCase() + a.slice(1).replaceAll("-", " ")
                   }}</label>
                 </div>
               </div>
@@ -148,7 +123,7 @@ export default {
     };
   },
   async created() {
-    console.log('hi');
+    // console.log('house');
     const user = userService.getLoggedinUser();
     if (user) {
       this.user = user;
@@ -162,7 +137,7 @@ export default {
     } else {
       this.house = houseService.getEmptyHouse();
     }
-    console.log("created", this.house.amenities);
+    console.log("created", this.house);
   },
   computed: {
     rate() {
@@ -170,9 +145,8 @@ export default {
       if (this.house.reviews.length === 0) return "New";
       let sum = this.house.reviews.reduce((acc, { rate }) => (acc += rate), 0);
       sum /= this.house.reviews.length;
-      return `${sum} (${this.house.reviews.length} ${
-        this.house.reviews.length === 1 ? "Review" : "Reviews"
-      })`;
+      return `${sum} (${this.house.reviews.length} ${this.house.reviews.length === 1 ? "Review" : "Reviews"
+        })`;
     },
   },
   methods: {
@@ -180,7 +154,7 @@ export default {
       this.user = userService.getLoggedinUser();
       this.$store.commit({ type: "toggleMustLogin", bool: false });
     },
-    setImg({url,idx}){
+    setImg({ url, idx }) {
       this.house.imgUrls[idx] = url
     },
     checkAmenities(amenity) {
