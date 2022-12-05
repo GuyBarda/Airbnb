b6291efec3d66b88a1311002c1e2cda0cea3f452
ShonChassel <gershoni1234@gmail.com>
2022-12-05 18:54:24 +0200

    first

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -30,8 +30,8 @@ export const userService = {
     remove,
     update,
     changeScore,
-    setWishlist
-    getTripsByUserId,
+    setWishlist,
+    getTripsByUserId
 };
 
 window.userService = userService;
