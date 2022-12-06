15d8aee9ecb9e3976a53be3233f841e77c756602
Guy Barda <guy.barda.1234@gmail.com>
2022-12-06 16:35:22 +0200

    refactor

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -5,62 +5,28 @@
             <p>airbxb</p>
         </div>
 
-<<<<<<< HEAD
-        <stay-search
-            :date="currDate"
-            :dest="currDest"
-            @toggleSearch="openZone"
-            :class="{ close: isOpen }"
-            @click="toggleSearch($event, key)"
-        />
-        <search-modal
-            @setDate="setDate"
-            @setDest="setDest"
-            @updateZone="openZone"
-            :zone="zone"
-            :class="{ open: isOpen }"
-        />
-=======
         <stay-search :date="currDate" :dest="currDest" @toggleSearch="openZone" :class="{ close: isOpen }"
             @click="toggleSearch($event, key)" />
         <search-modal @setDate="setDate" @setDest="setDest" @updateZone="openZone" :zone="zone"
             :class="{ open: isOpen }" />
->>>>>>> 9f71fe8c4c9febb19444a61c38f7048211f7fdb8
 
         <div class="user">
             <a class="become-host" href="/#/login">Become a host</a>
 
             <!-- <a href="/#/explore">
-<<<<<<< HEAD
                 <img class="explore-btn" src="../assets/svg/i18n.svg" />
             </a> -->
-=======
-                  <img class="explore-btn" src="../assets/svg/i18n.svg" />
-              </a> -->
->>>>>>> 9f71fe8c4c9febb19444a61c38f7048211f7fdb8
 
             <div class="user-menu-btn" @click="toggleUserMenu()">
                 <button>
                     <img class="menu-btn" src="../assets/svg/menu.svg" />
-<<<<<<< HEAD
-                    <img
-                        v-if="!user"
-                        class="host-image"
-                        src="https://res.cloudinary.com/nisan/image/upload/v1658872030/air2b/unprofile_ji7zus.png"
-                    />
-=======
                     <img v-if="!user" class="host-image"
                         src="https://res.cloudinary.com/nisan/image/upload/v1658872030/air2b/unprofile_ji7zus.png" />
->>>>>>> 9f71fe8c4c9febb19444a61c38f7048211f7fdb8
                     <img v-else class="host-image" :src="user.imgUrl" />
                 </button>
             </div>
 
-<<<<<<< HEAD
             <userMenu v-if="isMenu" @closeMenu="closeMenu" />
-=======
-            <userMenu v-if="isMenu" />
->>>>>>> 9f71fe8c4c9febb19444a61c38f7048211f7fdb8
         </div>
     </header>
 </template>
@@ -105,18 +71,16 @@ export default {
         // },
 
         toggleSearch(event) {
-            if (event.target.innerText === "Any week") {
-                this.$emit("clickDate");
-            }
-            console.log("hi");
+            if (event.target.innerText === "Any week") this.$emit("clickDate");
             this.$store.commit({ type: "toggleSearch", bool: true });
         },
         toggleUserMenu() {
-            if (!this.isMenu) {
-                this.isMenu = true;
-            } else {
-                this.isMenu = false;
-            }
+            // if (!this.isMenu) {
+            //     this.isMenu = true;
+            // } else {
+            //     this.isMenu = false;
+            // }
+            this.isMenu = !this.isMenu
         },
         closeMenu() {
             this.isMenu = false
@@ -140,7 +104,6 @@ export default {
         },
         whichLayout() {
             const layout = this.$route.path.split("/")[1];
-
             return layout === "stay" || layout === 'dashboard' ? "secondary-container" : "main-container";
         },
     },
