ccc40c7629bd727e85343bc71a02019304d0b532
Guy Barda <guy.barda.1234@gmail.com>
2022-12-06 10:42:20 +0200

    dashboard

--- src/views/user-dashboard.vue
+++ src/views/user-dashboard.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="user-dashboard secondary-container">
         <nav class="dashboard-nav">
-            <button class="btn-nav" @click="whichToShow('orders')">orders</button>
-            <button class="btn-nav" @click="whichToShow('my trips')">my trips</button>
-            <button class="btn-nav" @click="whichToShow('my stays')">my stays</button>
-            <button class="btn-nav" @click="whichToShow('wishlist')">wishlist</button>
-            <button class="btn-nav" @click="whichToShow('add a stay')">add a stay</button>
-            <!-- <a href="#">ADD A STAY</a> -->
+            <button class="btn-nav" @click="changeType('orders')">orders</button>
+            <button class="btn-nav" @click="changeType('trips')">my trips</button>
+            <button class="btn-nav" @click="changeType('houses')">my houses</button>
+            <button class="btn-nav" @click="changeType('wishlist')">wishlist</button>
+            <!-- <button class="btn-nav" >add a stay</button> -->
+            <router-link class="btn-nav" to="/house/edit">add a stay</router-link>
         </nav>
-        <!-- <orders :user="userToShow" :orders="ordersToShow" /> -->
-        <!-- <my-trips :user="userToShow" :trips="tripsToShow" /> -->
-        <wishlist :user="userToShow" :wishlist="wishListToShow" />
+        <orders v-if="typeToShow === 'orders'" :user="userToShow" :orders="ordersToShow" />
+        <my-trips v-if="typeToShow === 'trips'" :user="userToShow" :trips="tripsToShow" />
+        <wishlist v-if="typeToShow === 'wishlist'" :user="userToShow" :wishlist="wishListToShow" />
     </div>
 </template>
 
@@ -28,19 +28,21 @@ export default {
             user: null,
             orders: null,
             trips: null,
-            wishlist: null
+            wishlist: null,
+            type: 'trips'
         }
     },
     async created() {
         const { _id } = await userService.getLoggedinUser()
-        console.log('from session storage', _id)
         this.user = await userService.getById(_id)
         this.orders = await orderService.getOrdersByUserId(this.user._id)
-        // console.log(this.orders)
         this.trips = await userService.getTripsByUserId(this.user._id)
-        console.log('wishlist', this.user.wishlist)
         this.wishlist = this.user.wishlist
-
+    },
+    methods: {
+        changeType(str) {
+            this.type = str
+        }
     },
     computed: {
         userToShow() {
@@ -54,6 +56,9 @@ export default {
         },
         wishListToShow() {
             return this.wishlist
+        },
+        typeToShow() {
+            return this.type
         }
     },
     components: {
