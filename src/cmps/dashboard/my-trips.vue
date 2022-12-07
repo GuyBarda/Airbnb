82d1c9e178d49e4ebc0d60b1363cb633485df0bf
Guy Barda <guy.barda.1234@gmail.com>
2022-12-07 15:56:02 +0200

    dashboard

--- src/cmps/dashboard/my-trips.vue
+++ src/cmps/dashboard/my-trips.vue
@@ -1,15 +1,16 @@
 <template>
-    <div v-if="user" class="trips-container">
+    <div v-if="(user && trips && currTrip)" class="trips-container">
         <!-- <h2>Hi {{ user.fullname }}, you have {{ 2 }} pending trips</h2> -->
         <trip-list @openToDisplay="setTripDisplay" :trips="trips" />
         <section class="trip-display">
-            <details-map v-if="currTrip" class="map-for-dashboard" :lat="getTripLocLat" :lng="getTripLocLng" :title="currTrip.stay.loc.address"/>
+            <details-map v-if="currTrip" class="map-for-dashboard" :lat="getTripLocLat" :lng="getTripLocLng"
+                :title="currTrip.stay.loc.address" />
             <main class="trip-info">
-                <h3>{{currTrip.stay.loc.address}}</h3>
-                <p>Dates: {{currTrip.startDate}} - {{currTrip.endDate}}</p>
-                <p>Guests: {{currTrip.guests.adults}}</p>
-                <p>Total price: {{currTrip.totalPrice}}</p>
-                <p class="last">Order Status: {{currTrip.status}}</p>
+                <h3>{{ currTrip?.stay?.loc?.address }}</h3>
+                <p>Dates: {{ currTrip?.startDate }} - {{ currTrip?.endDate }}</p>
+                <p>Guests: {{ currTrip?.guests?.adults }}</p>
+                <p>Total price: {{ currTrip?.totalPrice }}</p>
+                <p class="last">Order Status: {{ currTrip.status }}</p>
             </main>
         </section>
     </div>
@@ -24,13 +25,14 @@ export default {
         trips: Array,
         user: Object
     },
-    created(){
+    created() {
         console.log(this.trips);
+        if (!this.trips) return
         this.currTrip = this.trips[0]
         console.log(this.currTrip);
     },
-    data(){
-        return{
+    data() {
+        return {
             currTrip: ''
         }
     },
@@ -38,16 +40,16 @@ export default {
         tripList,
         detailsMap
     },
-    methods:{
-        setTripDisplay(trip){
+    methods: {
+        setTripDisplay(trip) {
             this.currTrip = trip
         }
     },
-    computed:{
-        getTripLocLat(){
+    computed: {
+        getTripLocLat() {
             return this.currTrip.stay.loc.position.lat
         },
-        getTripLocLng(){
+        getTripLocLng() {
             return this.currTrip.stay.loc.position.lng
         }
     }
