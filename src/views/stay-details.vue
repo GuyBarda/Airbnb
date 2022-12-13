e8199e4c4e44ecad353e38f80aaa415f610a0056
Eyal Yehiel <eyalyhi@gmail.com>
2022-12-13 11:37:43 +0200

    fixed intersection

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -215,7 +215,9 @@ export default {
     },
     mounted() {
         setTimeout(() => {
-            const imgsContainer = this.$refs.imgsContainer;
+            // const imgsContainer = this.$refs.imgsContainer;
+            const imgsContainer = document.querySelector('.imgs-container')
+            
             const elReserveBtn = document.querySelector(
                 ".reserve-modal .btn-reserve"
             );
