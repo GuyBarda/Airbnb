2d6998b192085567ebb0de25cf5f4674a6653326
Guy Barda <guy.barda.1234@gmail.com>
2023-02-12 10:36:52 +0200

    added stay book template

--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,78 @@
-import { createRouter, createWebHistory } from "vue-router";
-import stayEdit from "../views/stay-edit.vue";
-import stayDetails from "../views/stay-details.vue";
-import stayApp from "../views/stay-app.vue";
-import userDashboard from "../views/user-dashboard.vue";
-import stayUpdate from "../views/stay-update.vue";
-import myOrders from '../cmps/dashboard/orders.vue'
-import myStays from '../cmps/dashboard/my-stays.vue'
-import myTrips from '../cmps/dashboard/my-trips.vue'
-import wishlist from '../cmps/dashboard/wishlist.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import stayEdit from '../views/stay-edit.vue';
+import stayDetails from '../views/stay-details.vue';
+import stayApp from '../views/stay-app.vue';
+import stayBook from '../views/stay-book.vue';
+import userDashboard from '../views/user-dashboard.vue';
+import stayUpdate from '../views/stay-update.vue';
+import myOrders from '../cmps/dashboard/orders.vue';
+import myStays from '../cmps/dashboard/my-stays.vue';
+import myTrips from '../cmps/dashboard/my-trips.vue';
+import wishlist from '../cmps/dashboard/wishlist.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: "/",
-            name: "home",
+            path: '/',
+            name: 'home',
             component: stayApp,
         },
         {
-            path: "/explore",
-            name: "explore",
+            path: '/book',
+            name: 'book',
+            component: stayBook,
+        },
+        {
+            path: '/explore',
+            name: 'explore',
             component: stayApp,
         },
         {
-            path: "/stay/:id",
-            name: "details",
+            path: '/stay/:id',
+            name: 'details',
             component: stayDetails,
         },
         {
-            path: "/stay/update/:id?",
-            name: "update",
+            path: '/stay/update/:id?',
+            name: 'update',
             component: stayUpdate,
         },
         {
-            path: "/stay/edit/:id?",
-            name: "edit",
+            path: '/stay/edit/:id?',
+            name: 'edit',
             component: stayEdit,
         },
         {
-            path: "/dashboard/host/:type?",
-            name: "dashboard-host",
+            path: '/dashboard/host/:type?',
+            name: 'dashboard-host',
             component: userDashboard,
             children: [
                 {
-                    path: "orders",
-                    name: "orders",
+                    path: 'orders',
+                    name: 'orders',
                     component: myOrders,
                 },
                 {
-                    path: "stays",
-                    name: "stays",
+                    path: 'stays',
+                    name: 'stays',
                     component: myStays,
                 },
             ],
         },
         {
-            path: "/dashboard/buyer/:type?",
-            name: "dashboard-buyer",
+            path: '/dashboard/buyer/:type?',
+            name: 'dashboard-buyer',
             component: userDashboard,
             children: [
                 {
-                    path: "trips",
-                    name: "trips",
+                    path: 'trips',
+                    name: 'trips',
                     component: myTrips,
                 },
                 {
-                    path: "wishlist",
-                    name: "wishlist",
+                    path: 'wishlist',
+                    name: 'wishlist',
                     component: wishlist,
                 },
             ],
---
+++ src/views/stay-book.vue
@@ -0,0 +1,83 @@
+<template>
+    <section class="stay-book secondary-container full">
+        <header>
+            <!-- <router-link></router-link> -->
+            <h1>Confirm and pay</h1>
+        </header>
+        <div class="info">
+            <div class="your-trip">
+                <h3>Your trip</h3>
+                <p>Dates</p>
+                <span>{{ 'may 9' }}</span>
+                <button>Edit</button>
+                <p>Geusts</p>
+                <span>{{ '1 guest' }}</span>
+                <button>Edit</button>
+            </div>
+            <div class="choose-payment">
+                <div class="group">
+                    <label>
+                        <div>
+                            <p>Pay in full</p>
+                            <input type="radio">
+                        </div>
+                        <span>Pay the total now and you're all set.</span>
+                    </label>
+                    <label>
+                        <div>
+                            <p>Pay part now, part later</p>
+                            <input type="radio">
+                        </div>
+                        <span>Pay $2,395.66 now, and the rest ($2,328.96) will be automatically charged to the same
+                            payment method on Apr 24, 2023. No extra fees.</span>
+                    </label>
+                </div>
+            </div>
+            <div class="pay-with">
+                <input type="text">
+                <div class="pay-with-group">
+                    <input type="text">
+                    <input type="text">
+                    <input type="text">
+                </div>
+                <input type="text">
+                <select name="" id=""></select>
+                <button>Enter a coupon</button>
+            </div>
+            <div class="cancellation">
+                <h3>Cancellation policy</h3>
+                <p>Free cancellation before Apr 9. Cancel before May 2 for a partial refund.</p>
+            </div>
+            <div class="ground-rules">
+                <h3>Ground rules</h3>
+                <p>We ask every guest to remember a few simple things about what makes a great guest.</p>
+                <ul>
+                    <li>Follow the house rules</li>
+                    <li>Treat your Host's home like your own</li>
+                </ul>
+            </div>
+            <button>Confirm and pay</button>
+        </div>
+        <div class="sticky">
+            <div class="">
+                <img src="" alt="">
+                <p>{{ stay?.type }}</p>
+                <p>{{ stay?.name }}</p>
+                <!-- <review></review> -->
+            </div>
+            <div class="">
+                <p>Your booking is protected by AirCover</p>
+            </div>
+            <div class="">
+                <h2>Price details</h2>
+                <div class=""></div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
