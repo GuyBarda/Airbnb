b297a0c0f0c069965e2425046df2d79b46bd117e
Eyal Yehiel <eyalyhi@gmail.com>
2023-02-12 18:53:36 +0200

    changed the filter btns

--- src/cmps/filter-btns.vue
+++ src/cmps/filter-btns.vue
@@ -1,5 +1,40 @@
 <template>
-    <carousel class="carousel" :settings="settings" :breakpoints="breakpoints">
+    <section class="filter-wrapper">
+        <button
+            :class="{ hidden: hidden === 'left' }"
+            @click="scroll('left')"
+            class="scroll left"
+        >
+            <arrow-left />
+        </button>
+        <section ref="btns" class="filter-btns">
+            <article
+                @click="setSort(btn.key)"
+                :class="{ selected: isSelected === btn.key }"
+                v-for="btn in btns"
+                :key="btn"
+            >
+                <img :src="btn.url" alt="" />
+                <p>{{ btn.key }}</p>
+            </article>
+        </section>
+        <button
+            :class="{ hidden: hidden === 'right' }"
+            @click="scroll('right')"
+            class="scroll right"
+        >
+            <arrow-right />
+        </button>
+        <!-- <section class="scrollers">
+            <button  @click="scroll('left')" class="scroll left">
+                <arrow-left />
+            </button>
+            <button @click="scroll('right')" class="scroll right">
+                <arrow-right />
+            </button>
+        </section> -->
+    </section>
+    <!-- <carousel class="carousel" :settings="settings" :breakpoints="breakpoints">
         <slide v-for="slide in btns" :key="slide">
 
             <div class="slide-wrapper" @click="setSort(slide.key)">
@@ -18,12 +53,14 @@
                 <navigation :slideWidth="100" />
             </div>
         </template>
-    </carousel>
+    </carousel> -->
 </template>
 
 <script>
-import "vue3-carousel/dist/carousel.css";
-import { Carousel, Slide, Navigation } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css"
+import { Carousel, Slide, Navigation } from "vue3-carousel"
+import arrowLeft from "../assets/svg/arrow-left.vue"
+import arrowRight from "../assets/svg/arrow-right.vue"
 
 export default {
     props: {
@@ -31,9 +68,10 @@ export default {
     },
     data() {
         return {
+            hidden: "left",
             isSelected: null,
             filterBy: {
-                type: '',
+                type: "",
             },
             settings: {
                 itemsToShow: 4,
@@ -58,16 +96,43 @@ export default {
     },
     methods: {
         setSort(type) {
-            if (this.$store.getters.isLoading) return;
-            this.filterBy.type = type;
-            this.isSelected = type;
-            this.$emit("filtered", this.filterBy);
+            if (this.$store.getters.isLoading) return
+            this.filterBy.type = type
+            this.isSelected = type
+            this.$emit("filtered", this.filterBy)
+        },
+
+        scroll(direction) {
+            const { scrollLeft, clientWidth, scrollWidth } = this.$refs.btns
+
+            const scrollTo = Math.round(
+                direction === "left"
+                    ? scrollLeft - clientWidth
+                    : scrollLeft + clientWidth
+            )
+            this.$refs.btns.scrollTo({
+                left: scrollTo,
+                behavior: "smooth",
+            })
+
+            if (scrollTo <= 0) this.hidden = "left"
+            else this.hidden = "between"
+            if (scrollTo + clientWidth >= scrollWidth) this.hidden = "right"
+            console.log("scrollTo", scrollTo)
+            // if (this.$refs.btns.scrollLeft === 0) this.hidden = "left"
+            // else this.hidden = "between"
+            // if (scrollLeft === 0) this.hidden = "left"
+            // else this.hidden = "between"
+            // console.log("scrollLeft", scrollLeft)
         },
     },
+    computed: {},
     components: {
         Carousel,
         Slide,
         Navigation,
+        arrowRight,
+        arrowLeft,
     },
-};
+}
 </script>
