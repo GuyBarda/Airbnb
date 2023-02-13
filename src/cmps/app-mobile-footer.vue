8f478e57f696e4c13baa93b504abc3727a4987b1
ShonChassel <gershoni1234@gmail.com>
2023-02-13 18:04:02 +0200

    complet airbnb

--- src/cmps/app-mobile-footer.vue
+++ src/cmps/app-mobile-footer.vue
@@ -1,47 +1,22 @@
 <template>
     <div class="app-mobile-footer" :class="whichContainer" :style="whichLayout">
         <section class="wrapper">
-            <router-link class="icon-container" @click="goToHome">
-                <img
-                    v-if="isMenu"
-                    src="../../src/assets/svg/search1.svg"
-                    alt=""
-                />
-                <img
-                    v-if="!isMenu"
-                    src="../../src/assets/svg/search2.svg"
-                    alt=""
-                />
+            <router-link class="icon-container" to="/" @click="handelClick()">
+                <img v-if="isMenu" src="../../src/assets/svg/search1.svg" alt="" />
+                <img v-if="!isMenu" src="../../src/assets/svg/search2.svg" alt="" />
                 <h1>Explore</h1>
             </router-link>
 
-            <router-link
-                class="icon-container"
-                @click="handelClick()"
-                to="/dashboard/buyer/wishlist"
-            >
+            <router-link class="icon-container" @click="handelClick()" to="/dashboard/buyer/wishlist">
                 <img v-if="isMenu" src="../../src/assets/svg/love.svg" alt="" />
-                <img
-                    v-if="!isMenu"
-                    src="../../src/assets/svg/love2.svg"
-                    alt=""
-                />
+                <img v-if="!isMenu" src="../../src/assets/svg/love2.svg" alt="" />
                 <h1>Wishlist</h1>
             </router-link>
 
             <div class="icon-container" @click="openLogIn()">
-                <img
-                    v-if="!loggedIn"
-                    src="../../src/assets/svg/user.svg"
-                    alt=""
-                />
-                <img
-                    class="user-img"
-                    v-else
-                    :src="loggedInUser?.imgUrl"
-                    alt=""
-                />
-                <h1>{{ loggedInUser?.username || "Log in" }}</h1>
+                <img v-if="!loggedIn" src="../../src/assets/svg/user.svg" alt="" />
+                <img class="user-img" v-else :src="loggedInUser?.imgUrl" alt="">
+                <h1>{{loggedInUser?.username || 'Log in'}}</h1>
             </div>
         </section>
     </div>
@@ -52,35 +27,32 @@ export default {
     data() {
         return {
             isMenu: true,
-            loggedIn: null,
-        };
+            loggedIn: null
+        }
     },
     created() {
         this.isMenu = true;
     },
     methods: {
         openLogIn() {
-            if (this.loggedIn) return;
+            if(this.loggedIn) return
             this.$store.commit({ type: "toggleLogInModal", bool: true });
         },
-        goToHome() {
-            this.isMenu = true;
-            this.$router.push("/");
-        },
-
-        handelClick() {
+        handelClick(){
             this.isMenu = !this.isMenu;
-        },
+        }
     },
-    computed: {
-        loggedInUser() {
-            return this.$store.getters.loggedinUser;
-        },
+    computed:{
+    loggedInUser(){
+        return this.$store.getters.loggedinUser
+    }
     },
-    watch: {
-        loggedInUser(user) {
-            this.loggedIn = user ? true : false;
-        },
-    },
-};
+     watch:{loggedInUser(user){
+        this.loggedIn = user ? true : false
+    }
+}
+}
+
+
+
 </script>
