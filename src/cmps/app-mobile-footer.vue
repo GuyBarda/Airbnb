d88a6ddb3f2b773abad02306b774340cf1e7fd47
Eyal Yehiel <eyalyhi@gmail.com>
2023-02-13 17:44:39 +0200

    added styling to footer in mobile and to dashboard in mobile

--- src/cmps/app-mobile-footer.vue
+++ src/cmps/app-mobile-footer.vue
@@ -1,32 +1,48 @@
 <template>
     <div class="app-mobile-footer" :class="whichContainer" :style="whichLayout">
         <section class="wrapper">
-            <div class="icon-container">
-                <img src="../../src/assets/svg/serch.svg" alt="">
+            <router-link to="/" class="icon-container">
+                <img src="../../src/assets/svg/serch.svg" alt="" />
                 <h1>Explore</h1>
-            </div>
+            </router-link>
 
             <router-link class="icon-container" to="/dashboard/buyer/wishlist">
-                <img src="../../src/assets/svg/love.svg" alt="">
+                <img src="../../src/assets/svg/love.svg" alt="" />
                 <h1>Wishlist</h1>
             </router-link>
 
             <div class="icon-container" @click="openLogIn()">
-                <img src="../../src/assets/svg/user.svg" alt="">
-                <h1>Log in</h1>
+                <img v-if="!loggedIn" src="../../src/assets/svg/user.svg" alt="" />
+                <img class="user-img" v-else :src="loggedInUser?.imgUrl" alt="">
+                <h1>{{loggedInUser?.username || 'Log in'}}</h1>
             </div>
-
         </section>
     </div>
 </template>
 
 <script>
 export default {
-    computed: {},
+    data() {
+        return {
+            loggedIn: null
+        }
+    },
     methods: {
         openLogIn() {
+            if(this.loggedIn) return
             this.$store.commit({ type: "toggleLogInModal", bool: true });
         },
-}        
+},
+computed:{
+    loggedInUser(){
+        return this.$store.getters.loggedinUser
+    }
+},
+watch:{
+    loggedInUser(user){
+        this.loggedIn = user ? true : false
+    }
+}
+
 };
 </script>
